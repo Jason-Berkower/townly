eaf3254ae07c136529a62f70b36e150d0a4292f9
Jason Berkower <jasonberkower@Jasons-iMac.home>
2021-04-16 13:03:51 -0400

    Add boilerplate functionality to database index.js.

---
+++ db/index.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("http://127.0.0.1:27017/tourDatabase", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB Server Connected!");
+  })
+  .catch((error) => {
+    console.log("MongoDB Server Connection Error:", error.message);
+  });
+
+// Take this out before deployment
+mongoose.set("debug", true);
+
+const db = mongoose.connection;
+
+module.exports = db;
