7de4ab3ba7e4629db39ec80470fad9ffd1b2cc89
Peco Dory <pecodory@Pecos-MBP.attlocal.net>
2021-04-18 13:50:51 -0400

    added restrict routes

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+const { Route } = require("express");
+const {
+  signUp,
+  signIn,
+  verify,
+  changePassword,
+} = require("../controllers/users");
+const {
+  getAllTours,
+  getOneTour,
+  createTour,
+  updateTour,
+  deleteTour,
+} = require("../controllers/tours");
+const restrict = require("../helpers/restricted.js");
+
+const router = Router();
+
+// For Users
+router.post("/sign-up", signUp);
+router.post("/sign-in", signIn);
+router.get("/verify", verify);
+router.put("/change-password/:id", changePassword);
+
+// For Tours
+router.get("/", getAllTours);
+router.get("/:id", getOneTour);
+router.post("/", restrict, createTour );
+router.put("/:id",restrict, updateTour );
+router.delete("/:id", restrict, deleteTour);
+
+module.exports = router;
