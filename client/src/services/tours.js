26717439dfd4f4d7fdb37387b87e4722d1fd6238
Jason Berkower <jasonberkower@Jasons-iMac.home>
2021-04-19 17:02:27 -0400

    Added CRUD functions to services/tours.js.

---
+++ client/src/services/tours.js
@@ -0,0 +1,30 @@
+import api from "./apiConfig";
+
+export const getAllTours = async () => {
+  try {
+    const res = await api.get("/tours");
+    return res.data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const getTourById = async (id) => {
+  let res = await api.get(`/tours/${id}`);
+  return res.data;
+};
+
+export const createTour = async (tour) => {
+  let res = await api.post("/tours", tour);
+  return res.data;
+};
+
+export const updateTour = async (id) => {
+  let res = await api.put(`/tours/${id}`);
+  return res.data;
+};
+
+export const deleteTour = async (id) => {
+  let res = await api.delete(`/tours/${id}`);
+  return res.data;
+};
