1b1d586aee9c170714d234dc9a560c4a7f82b88d
Amanda <amandalphail@gmail.com>
2021-04-19 16:10:35 -0400

    created createTour screen and wrote Layout Nav and Footer components

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Footer() {
+
+  return (
+    <div>
+      <div id="projectLink">
+
+      <Link to="">Checkout the Project!</Link>
+      </div>
+      <div id="devLinks">
+        
+      <h4>Check Out the Developers Works!</h4>
+      <Link to="">Amanda</Link>
+      <Link to="">Jason</Link>
+      <Link to="">Paco</Link>
+      </div>
+    </div>
+  )
+}
