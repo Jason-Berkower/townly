9f9490c0aad1a56dc3ebe14297708f61d4496c7a
Amanda <amandalphail@gmail.com>
2021-04-19 17:21:49 -0400

    create useState and beginnings of form displayed on page

--- client/src/screens/CreateTour.jsx
+++ client/src/screens/CreateTour.jsx
@@ -1,10 +1,51 @@
 import { useState, useEffect } from "react"
 import { createTour } from "../services/tours"
+import {useHistory} from "react-router-dom"
 
-export default function CreateTour() {
+export default function CreateTour(props) {
+  let [tour, setTour] = useState({
+    name: "",
+    tour_operator: `${props.currentUser}`,
+    date: "",
+    time: "",
+    type: "",
+    description: "",
+    price: "",
+    popularity: 0,
+    imgURL: "",
+  })
+
+//handleChange
+//handleSubmit
+//selector for time, type, price
+  //imgURL - radio button of 3-5 images per type to select from
+  
   return (
     <div>
-      
+      <Layout>
+        <h2>Post Your Tour</h2>
+        <form onSubmit={handleSubmit}>
+          <input id="name" placeholder="Tour Name" type="text" value={tour.name} onChange={handleChange} />
+
+          <label htmlFor="date">Date: </label>
+          <input id="date" type="date" value="tour.date" onChange={handleChange} />
+
+          <input id="time" placeholder="Tour Time" type="text" value={tour.time} onChange={handleChange} />
+
+          <input id="type" placeholder="Tour Type" type="text" value={tour.type} onChange={handleChange} />
+
+          <input id="description" placeholder="description" type="text" value={tour.description} onChange={handleChange} />
+          
+          <input id="price" placeholder="Price" type="text" value={tour.price} onChange={handleChange} />
+          
+          <input id="image" placeholder="Image URL" type="text" value={tour.imgURL} onChange={handleChange} />
+
+          <input type = "submit"/>
+
+          
+
+        </form>
+      </Layout>
     </div>
   )
 }
