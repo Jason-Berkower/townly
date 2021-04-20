dbed210461f471cb4e9039025fd2bdca14990a8b
Amanda <amandalphail@gmail.com>
2021-04-20 18:29:30 -0400

    axios call to get tours and pass props

--- client/src/components/GalleryContainer.jsx
+++ client/src/components/GalleryContainer.jsx
@@ -1,15 +1,34 @@
 import TopTour from "./TopTour"
+import { useState, useEffect } from "react";
+import { getAllTours } from "../services/tours";
+
 export default function GalleryContainer() {
   //map through all tours
   //if tour.popularity =< 8 then <tour pass props>
   
+  const [tours, setTours] = useState([]);
+
+  useEffect(() => {
+    handleRequest();
+  }, []);
+
+  const handleRequest = async () => {
+    const data = await getAllTours();
+    // console.log(data)
+    setTours(data);
+  };
+
+  console.log(tours)
+
   return (
     <div>
       <h2>Our Top Tours</h2>
       <div id="gallery">
         <button>Back</button>
         <div id="tourcontainer">
-          <TopTour/>
+          {tours.map((tour, index) => {
+            return <TopTour key={index} tour={tour}/>
+          })}
         </div>
         <button>Next</button>
       </div>
--- client/src/components/TopTour.jsx
+++ client/src/components/TopTour.jsx
@@ -4,10 +4,16 @@ export default function Tour(props) {
 //props passed down of top tours info
   //print top tour - name image location 
   //have show details button that takes you to that tours details page that paco is setting up
+let TopTour = props.tour
 
+// if (tour.popularity <= "8") {
+//   console.log(tour.tour_operator)
+  console.log(TopTour)
+  
   return (
     <div>
       TopTour
+       {/* {TopTour.name} */}
     </div>
   )
 }
