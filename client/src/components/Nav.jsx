3e1334dfb5f981aa1f1013820099252f3a4d1a4a
Amanda <amandalphail@gmail.com>
2021-04-20 11:59:04 -0400

    started styling on nav

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,4 +1,5 @@
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
+import "./CSS/nav.css"
 
 export default function Nav(props) {
   const displayLinks = () => {
@@ -10,7 +11,7 @@ export default function Nav(props) {
       );
     } else {
       return (
-        <div>
+        <div id="account">
             <Link to="/sign-in">Sign In</Link>
             <Link to="/sign-up">Sign Up</Link>
         </div>
@@ -22,15 +23,16 @@ export default function Nav(props) {
     <div id="nav">
       <h1>Townly</h1>
       <div id="links">
-        <Link to="/">Home</Link>
-        <Link to="/tours">All Tours</Link>
-        <Link to="/create-tour">
-          Post Tour
-        </Link>
-        {/* <Link to="/sign-up">Sign Up</Link>
-        <Link to="/sign-in">Sign In</Link>
-      <Link to="/sign-out">Sign Out</Link> */}
-        {displayLinks()}
+        <div id="everyoneslinks">
+          <Link to="/">Home</Link>
+          <Link to="/tours">All Tours</Link>
+          <Link to="/create-tour">
+            Post Tour
+          </Link>
+        </div>
+        <div>
+          {displayLinks()}
+        </div>
     </div>
       
     </div>
