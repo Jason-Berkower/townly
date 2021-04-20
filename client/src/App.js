5ab2b5b2608c9877e8c5ae8b94acf7c550c149c4
Peco Dory <pecodory@Pecos-MBP.attlocal.net>
2021-04-20 01:47:57 -0400

    starting routes and add edit to detail

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import TourDetails from "./screens/TourDetails";
+import {Route, Link} from "react-router-dom"
+
+
+
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Route exact path="/">
+    <div>home</div>
+    </Route>
+    <Route exact path="/tours/:id">
+    <TourDetails />
+    </Route>
+    
+  </div>;
 }
 
 export default App;
--- client/src/screens/TourDetails.jsx
+++ client/src/screens/TourDetails.jsx
@@ -1,26 +1,25 @@
 import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
-import { getTourById } from "../auth/product";
+import { getTourById } from "../services/tours";
 
 export default function TourDetails(props) {
   const { id } = useParams();
   const [tourDetails, setTourDetails] = useState({});
-
   useEffect(() => {
     getTourDetails();
   }, []);
 
   const getTourDetails = async () => {
-    const data = await getTourById(id);
+    const data = await getTourById( id );
     setTourDetails(data);
   };
+  console.log(props);
 
   return (
     <div>
-      <h2>{tourDetails.name}</h2>
+      <h1>{tourDetails.name}</h1>
       <h3>{tourDetails.type}</h3>
-      <img src={tourDetails.imgURL} alt="" />
-      {/* string interpolation for accessibility? */}
+      <img src={tourDetails.imgURL} alt={`${tourDetails.type} tour`} />
       <h5>{tourDetails.tour_operator}</h5>
       <p>{tourDetails.description}</p>
       <p>
@@ -31,6 +30,7 @@ export default function TourDetails(props) {
       {props.currentUser && (
         //have to check current user is same as user who created tour
         <Link to={`/edit-tour/${tourDetails._id}`}>Edit Tour</Link>
+        //create edit link
       )}
     </div>
   );
