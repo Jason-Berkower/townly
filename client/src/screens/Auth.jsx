2809bb92607925ab3f4f6da61c63ca1a73d2a729
Jason Berkower <jasonberkower@Jasons-iMac.home>
2021-04-21 13:13:06 -0400

    Non-tourGuide users can now log out.

--- client/src/screens/Auth.jsx
+++ client/src/screens/Auth.jsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import SignIn from '../components/SignIn';
 import SignUp from '../components/SignUp';
 
@@ -7,13 +6,13 @@ export default function Auth(props) {
 
   return (
     <div>
-        <div id='sign-in'>
-          <SignIn setCurrentUser={props.setCurrentUser} setTourGuide={props.setTourGuide} />
-        </div>
-        <div id='separator' />
-        <div id='sign-up'>
-          <SignUp setCurrentUser={props.setCurrentUser} setTourGuide={props.setTourGuide}/>
-        </div>
+      <div id='sign-in'>
+        <SignIn setCurrentUser={props.setCurrentUser} setTourGuide={props.setTourGuide} />
+      </div>
+      <div id='separator' />
+      <div id='sign-up'>
+        <SignUp setCurrentUser={props.setCurrentUser} setTourGuide={props.setTourGuide} />
+      </div>
     </div>
   );
 }
