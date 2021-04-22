4473e13c88b0c41b0b26fff161541646e4869b23
Amanda <amandalphail@gmail.com>
2021-04-21 22:47:57 -0400

    form stylezzzz

--- client/src/screens/CreateTour.jsx
+++ client/src/screens/CreateTour.jsx
@@ -67,15 +67,17 @@ export default function CreateTour(props) {
   return (
     <div>
 
-        <h2>Post Your Tour</h2>
-        <form id="tourform" onSubmit={handleSubmit}>
-          <input id="name" placeholder="Tour Name" type="text" value={tour.name} onChange={handleChange} />
-
+      <h2>Post Your Tour</h2>
+      <form id="tourform" onSubmit={handleSubmit}>
+        <input className="tourinput" id="name" placeholder="Tour Name" type="text" value={tour.name} onChange={handleChange} />
+        
+        <div className="width">
           <label htmlFor="date">Date: </label>
-          <input id="date" type="date" value={tour.date} onChange={handleChange} />
+          <input className="tourinput" id="date" type="date" value={tour.date} onChange={handleChange} />
+        </div>
 
-          {/* <input id="time" placeholder="Tour Time" type="text" value={tour.time} onChange={handleChange} /> */}
-
+          {/* <input className="tourinput" id="time" placeholder="Tour Time" type="text" value={tour.time} onChange={handleChange} /> */}
+          <div>
           <label htmlFor="time">Tour Time: </label>
           <select id="time" form="tourform" value={tour.time} onChange={handleChange}>
             <option value="0">12 am</option>
@@ -103,9 +105,10 @@ export default function CreateTour(props) {
             <option value="22">10 pm</option>
             <option value="23">11 pm</option>
           </select>
+          </div>
 
-
-          {/* <input id="type" placeholder="Tour Type" type="text" value={tour.type} onChange={handleChange} /> */}
+        {/* <input className="tourinput" id="type" placeholder="Tour Type" type="text" value={tour.type} onChange={handleChange} /> */}
+        <div id="label">
           <label htmlFor="type">Tour Type: </label>
           <select id="type" form="tourform" value={tour.type} onChange={handleChange}>
             <option value="adventure">Adventure</option>
@@ -121,25 +124,26 @@ export default function CreateTour(props) {
             <option value="tasting">Tasting</option>
             <option value="personal">Personalized</option>
           </select>
+          </div>
 
-          <input id="description" placeholder="Description" type="text" value={tour.description} onChange={handleChange} />
+          <input className="tourinput" id="description" placeholder="Description" type="text" value={tour.description} onChange={handleChange} />
           
           <label htmlFor="price">$</label>
-          <input id="price" placeholder="Price" type="Number" value={tour.price} onChange={handleChange} />
+          <input className="tourinput" id="price" placeholder="Price" type="Number" value={tour.price} onChange={handleChange} />
           {/* <label htmlFor="price">$ </label>
           <select id="price" form="tourform" value={tour.price} onChange={handleChange}>
           <option value="5">5</option>
           </select> */}
           
-          <input id="imgURL" placeholder="Image URL" type="text" value={tour.imgURL} onChange={handleChange} />
+          <input className="tourinput" id="imgURL" placeholder="Image URL" type="text" value={tour.imgURL} onChange={handleChange} />
 
-          <input id="street" placeholder="Street" type="text" value={tour.street} onChange={handleChange} />
+          <input className="tourinput" id="street" placeholder="Street" type="text" value={tour.street} onChange={handleChange} />
 
-          <input id="city" placeholder="City" type="text" value={tour.city} onChange={handleChange} />
+          <input className="tourinput" id="city" placeholder="City" type="text" value={tour.city} onChange={handleChange} />
 
-          <input id="country" placeholder="Country" type="text" value={tour.country} onChange={handleChange} />
+          <input className="tourinput" id="country" placeholder="Country" type="text" value={tour.country} onChange={handleChange} />
 
-          <input type = "submit"/>
+          <input id="submit" type = "submit"/>
 
         </form>
     </div>
