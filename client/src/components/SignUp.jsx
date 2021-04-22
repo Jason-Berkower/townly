d8a5f6c0899708c2eafefdac2e716903c0aecef1
Jason Berkower <jasonberkower@Jasons-iMac.home>
2021-04-22 15:20:03 -0400

    Updating auth and nav styling.

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -44,19 +44,29 @@ export default function SignUp(props) {
   };
 
   return (
-    <div>
+    <div className='sign-up-container'>
       <h3>Sign Up</h3>
       <form onSubmit={handleSubmit}>
-        <label>Username</label>
-        <input name='username' value={input.username} placeholder='Enter username...' onChange={handleChange} />
-        <label>Email</label>
-        <input type='email' name='email' value={input.email} placeholder='Enter email...' onChange={handleChange} />
-        <label>Password</label>
-        <input type='password' name='password' value={input.password} placeholder='Enter password...' onChange={handleChange} />
-        <label>Confirm Password</label>
-        <input type='password' name='passwordConfirmation' value={input.passwordConfirmation} placeholder='Reenter password...' onChange={handleChange} />
-        <input type='checkbox' id='is_tourGuide' name='is_tourGuide' value={input.is_tourGuide} onChange={handleCheck} />
-        <label>Tour Guide</label><br />
+        <div id='username'>
+          <label>Username</label>
+          <input name='username' value={input.username} placeholder='Enter username...' onChange={handleChange} />
+        </div>
+        <div id='email'>
+          <label>Email</label>
+          <input type='email' name='email' value={input.email} placeholder='Enter email...' onChange={handleChange} />
+        </div>
+        <div id='password'>
+          <label>Password</label>
+          <input type='password' name='password' value={input.password} placeholder='Enter password...' onChange={handleChange} />
+        </div>
+        <div id='confirm-password'>
+          <label>Confirm Password</label>
+          <input type='password' name='passwordConfirmation' value={input.passwordConfirmation} placeholder='Reenter password...' onChange={handleChange} />
+        </div>
+        <div id='checkbox'>
+          <input type='checkbox' id='is_tourGuide' name='is_tourGuide' value={input.is_tourGuide} onChange={handleCheck} />
+          <label>Tour Guide</label><br />
+        </div>
         <button type='submit'>Sign Up</button>
       </form>
     </div>
