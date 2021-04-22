e22412b88078ab4286e28c1a24372abfb2065724
Peco Dory <pecodory@Pecos-MBP.attlocal.net>
2021-04-22 16:07:09 -0400

    detail css

--- client/src/screens/TourList.jsx
+++ client/src/screens/TourList.jsx
@@ -16,18 +16,18 @@ export default function ProductList(props) {
   };
 
   return (
-    <div>
+    <div className="tourList">
 
-      <h2>townly tours</h2>
+      <h1 className="listHeader">townly tours</h1>
       {tours.map((tour, index) => {
         return (
           <div key={index}>
-            <h3>{tour.name}</h3>
+            <h2>{tour.name}</h2>
             <h4>{tour.type} tour</h4>
             <h5>
               {tour.city}, {tour.country}
             </h5>
-            <img className="tour-list-img" src={tour.imgURL} alt="" />
+            <img className="tourListImg" src={tour.imgURL} alt="" />
             <div className="tour-edit">
               <Link to={`/tours/${tour._id}`}>show details</Link>
             </div>
